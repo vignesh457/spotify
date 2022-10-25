1ac5bc317a9f405f70a73305144fc892fc1b8f13
vignesh457 <91398734+vignesh457@users.noreply.github.com>
2022-10-25 16:56:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ let vol = document.getElementById("volume");
 let songIndex = 0;
 let colorArr = [[255, 88, 58],[255, 144, 50],[254, 221, 15],[75, 150, 62],[35, 67, 159],[86, 20, 117]];
 let colorState=0;
-let playState=false;
+let myInterval = setInterval(disco, 1000);
+stopDisco();
 //master play and pause
 masterPlay.addEventListener('click',()=>{
   if(audio.paused || audio.duration<=0){
     audio.play();
-    disco("play");
+    startDisco()
     masterPlay.src="./images/pause.png";
     stateToggle(songIndex,"pause","master");
   }
   else{
     audio.pause();
-    disco("pause");
+    stopDisco();
     masterPlay.src="./images/play.png";
     stateToggle(songIndex,"play","master");
   }
@@ -33,6 +34,19 @@ progressBar.addEventListener('change',()=>{
   let prev = audio.currentTime;
   audio.currentTime=(progressBar.value*audio.duration)/100;
 });
+//Time display
+setInterval(()=>{
+  //Total duration
+  let totalTime=document.getElementById("totalTime");
+  let tmin=Math.floor(audio.duration/60);
+  let tsec=Math.floor(audio.duration%60);
+  totalTime.innerHTML=`${tmin}:${(tsec<10)?"0":""}${tsec}`;
+  //Current duration
+  let currTime=document.getElementById("currTime");
+  let cmin=Math.floor(audio.currentTime/60);
+  let csec=Math.floor(audio.currentTime%60);
+  currTime.innerHTML=`${cmin}:${(csec<10)?"0":""}${csec}`;
+},1000);
 //volume control
 vol.addEventListener('change',()=>{
   audio.volume=vol.value/10;
@@ -51,6 +65,7 @@ Array.from(document.getElementsByClassName("card")).forEach((element)=>{
   customPlay.addEventListener('click',()=>{
       let tempIndex=Number.parseInt(element.getAttribute("id"));
       makeAllPlay();
+      let prevAudioPaused=audio.paused;
       if(tempIndex!=songIndex)
       {
         audio.src=`./songs/${tempIndex}.mp3`;
@@ -58,13 +73,14 @@ Array.from(document.getElementsByClassName("card")).forEach((element)=>{
       }
       if(audio.paused ){
         audio.play();
-        disco("play");
+        if(prevAudioPaused)
+          startDisco();
         customPlay.src="./images/pause-g.png";
         masterPlay.src="./images/pause.png";
       }else
       {
         audio.pause();
-        disco("pause");
+        stopDisco();
         customPlay.src="./images/play-g.png";
         masterPlay.src="./images/play.png";
       }
@@ -86,18 +102,24 @@ document.getElementById("next").addEventListener('click',nextFunction);
 function prevFunction(){
   stateToggle(songIndex,"play");
   songIndex=(songIndex==0)?7:(songIndex-1);
+  let prevAudioPaused=audio.paused;
   audio.src=`./songs/${songIndex}.mp3`;
   audio.play();
-  disco("play");
+  if(prevAudioPaused)
+    startDisco();
   stateToggle(songIndex,"pause","prev");
+  masterPlay.src="./images/pause.png";
 }
 function nextFunction(){
   stateToggle(songIndex,"play");
   songIndex=(songIndex==7)?0:(songIndex+1);
+  let prevAudioPaused=audio.paused;
   audio.src=`./songs/${songIndex}.mp3`;
   audio.play();
-  disco("play");
+  if(prevAudioPaused)
+    startDisco();
   stateToggle(songIndex,"pause","next");
+  masterPlay.src="./images/pause.png";
 }
 // stateToggle function()
 function stateToggle(id,state,location){
@@ -114,16 +136,25 @@ function stateToggle(id,state,location){
   let cardText = currId.getElementsByClassName("card-text")[0];
   statusBarText.innerHTML=cardText.innerHTML;
 }
-//Disco lighting
-function disco(playState){
 
-  var interval=setInterval(()=>{
-    let element = document.getElementsByClassName("song-container")[0];
-    let r = colorArr[colorState][0];
-    let g = colorArr[colorState][1];
-    let b = colorArr[colorState][2];
-    clearInterval(interval);
-    element.style.backgroundImage=`linear-gradient(rgba(${r},${g},${b},0.8) -8%, rgb(18, 18, 18) 70%)`;
-    colorState=(colorState+1)%6;
-  },1000);
+// disco timer start/stop Function
+function startDisco() {
+  myInterval = setInterval(disco, 200);
 }
+function stopDisco() {
+  colorState=3;
+  clearInterval(myInterval);
+}
+// Disco function
+function disco() {
+  let element = document.getElementsByClassName("song-container")[0];
+  let r = colorArr[colorState][0];
+  let g = colorArr[colorState][1];
+  let b = colorArr[colorState][2];
+  element.style.backgroundImage=`linear-gradient(rgba(${r},${g},${b},0.8) -8%, rgb(18, 18, 18) 70%)`;
+  colorState=(colorState+1)%6;
+}
+//favTab
+document.getElementById("favTab").addEventListener('click',()=>{
+  alert("You are already in the Favourites page\nHappy Listening...");
+});
